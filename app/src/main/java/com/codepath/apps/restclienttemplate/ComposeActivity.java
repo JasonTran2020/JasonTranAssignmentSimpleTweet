0528969ee57bde71c1ccd7dbe999753bc3a65413
JasonTran2020 <jasont787@yahoo.com>
2021-10-04 17:43:59 -0700

    Second part of Twitter

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,23 +3,36 @@ package com.codepath.apps.restclienttemplate;
 import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.RippleDrawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.parceler.Parcels;
+
+import okhttp3.Headers;
+
 public class ComposeActivity extends AppCompatActivity {
+    public static final String TAG="ComposeActivity";
     public static final int MAX_TWEET_LENGTH=280;
     EditText etCompose;
     Button btnTweet;
     TextView tvCharacterCount;
+    TwitterClient client;
     int color;
 
 
@@ -28,10 +41,15 @@ public class ComposeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
 
+        client=TwitterApp.getRestClient(this);
+
         etCompose=findViewById(R.id.etCompose);
         btnTweet=findViewById(R.id.btnTweet);
         //Saving current color of the text
         color=btnTweet.getCurrentTextColor();
+        RippleDrawable rippleDrawable = (RippleDrawable) btnTweet.getBackground();
+        Drawable.ConstantState state= rippleDrawable.getConstantState();
+
 
         tvCharacterCount=findViewById(R.id.tvCharacterCount);
         tvCharacterCount.setText(String.valueOf(0) + " out of " +String.valueOf(MAX_TWEET_LENGTH));
@@ -45,7 +63,7 @@ public class ComposeActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                tvCharacterCount.setText(String.valueOf(count) + " out of " +String.valueOf(MAX_TWEET_LENGTH));
+                tvCharacterCount.setText(String.valueOf(count+start) + " out of " +String.valueOf(MAX_TWEET_LENGTH));
                 //Make it red if it's beyound the max length
                 if (count>MAX_TWEET_LENGTH)
                     btnTweet.setTextColor(Color.parseColor("#FF0000"));
@@ -78,6 +96,27 @@ public class ComposeActivity extends AppCompatActivity {
                 }
                 Toast.makeText(ComposeActivity.this,tweetContent,Toast.LENGTH_SHORT).show();
                 // Make an API call
+                client.publishTweet( new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.i(TAG,"Onsuccess published tweet");
+                        try {
+                            Tweet tweet= Tweet.fromJson(json.jsonObject);
+                            //Log.i(TAG,"Published Tweet says" + tweet);
+                            Intent intent=new Intent();
+                            intent.putExtra("tweet", Parcels.wrap(tweet));
+                            setResult(RESULT_OK,intent);
+                            finish();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Log.e(TAG,"Fail to publish tweet", throwable);
+                    }
+                },tweetContent);
             }
         });
     }
--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,12 +1,14 @@
 package com.codepath.apps.restclienttemplate;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -14,10 +16,14 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.apps.restclienttemplate.models.TweetDao;
+import com.codepath.apps.restclienttemplate.models.TweetWithUser;
+import com.codepath.apps.restclienttemplate.models.User;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,19 +33,21 @@ import okhttp3.Headers;
 public class TimelineActivity extends AppCompatActivity {
 
     public static final String TAG="TimelineActivity";
+    private final int REQUEST_CODE=20;
     TwitterClient client;
     RecyclerView rvTweets;
     List<Tweet> tweets;
     TweetsAdapter adapter;
     SwipeRefreshLayout swipeContainer;
     EndlessRecyclerViewScrollListener scrollListener;
+    TweetDao tweetDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
         client=TwitterApp.getRestClient(this);
-
+        tweetDao = ((TwitterApp) getApplicationContext()).getMyDatabase().tweetDao();
 
         swipeContainer=findViewById(R.id.swipeContainer);
         //Change the color of the refresher
@@ -72,6 +80,19 @@ public class TimelineActivity extends AppCompatActivity {
             }
         };
         rvTweets.addOnScrollListener(scrollListener);
+
+        //Query for existing tweets in the DB
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                Log.i(TAG,"Showing data from database");
+                List<TweetWithUser> tweetWithUsers=tweetDao.recentItems();
+                List<Tweet> tweetsFromDB=TweetWithUser.getTweetList(tweetWithUsers);
+                adapter.clear();
+                adapter.addAll(tweetsFromDB);
+            }
+        });
+
         populateHomeTimeline();
     }
 
@@ -90,7 +111,7 @@ public class TimelineActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-
+                Log.e(TAG,"onFailure loadMoreData");
             }
         }, tweets.get(tweets.size()-1).id);
     }
@@ -108,22 +129,51 @@ public class TimelineActivity extends AppCompatActivity {
             //Compose icon selected
             //Navigate to the compose activity
             Intent intent=new Intent(this,ComposeActivity.class);
-            startActivity(intent);
+            startActivityForResult(intent,REQUEST_CODE);
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        if (requestCode==REQUEST_CODE && resultCode==RESULT_OK){
+            //Get data from intent (tweet)
+            Tweet tweet= Parcels.unwrap(data.getParcelableExtra("tweet"));
+            //Update recyler view the tweet.
+            tweets.add(0,tweet);
+            //Notify adapter
+            adapter.notifyItemInserted(0);
+            rvTweets.smoothScrollToPosition(0);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void populateHomeTimeline() {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
+
                 Log.i(TAG,"onSuccess"+ json.toString());
                 JSONArray jsonArray= json.jsonArray;
                 try {
+                    List<Tweet> tweetsFromNetwork=Tweet.fromJsonArray(jsonArray);
                     adapter.clear();
-                    adapter.addAll(Tweet.fromJsonArray(jsonArray));
+                    adapter.addAll(tweetsFromNetwork);
                     swipeContainer.setRefreshing(false);
+                    AsyncTask.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            Log.i(TAG,"Saving data into database");
+                            //Insert users first
+                            List<User> usersFromNetwork= User.fromJsonTweetArray(tweetsFromNetwork);
+                            tweetDao.insertModel(usersFromNetwork.toArray(new User[0]));
+                            //Then insert tweets
+                            tweetDao.insertModel(tweetsFromNetwork.toArray(new Tweet[0]));
+
+                        }
+                    });
                 } catch (JSONException e) {
                     Log.e(TAG,"Json exception",e);
                     e.printStackTrace();
@@ -132,7 +182,7 @@ public class TimelineActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                Log.i(TAG,"onFAilure"+response, throwable);
+                Log.i(TAG,"onFAilure for populate hometimeline"+response, throwable);
             }
         });
     }
